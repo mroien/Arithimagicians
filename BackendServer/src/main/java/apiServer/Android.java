f98b3591077e8130b932ba87f2b36e8b17d1daef
jacobkinzer01 <jacobkinzer01@gmail.com>
2015-10-13 15:33:14 -0500

    Added in base SQL code for backend server
    Will need to add in more calls for updates to leaderboard

--- BackendServer/src/main/java/apiServer/Android.java
+++ BackendServer/src/main/java/apiServer/Android.java
@@ -1,19 +1,79 @@
 package apiServer;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Calendar;
+
 public class Android {
 	public final String accountId;
 	public final String powerUp;
-	
+	private Connection conn;
+	private String dbUser;
+	private String dbPass;
+	private String ip;
 	public Android(){
 		this.accountId = "test";
 		this.powerUp = "test";
+		dbUser = "";
+		dbPass = "";
+		ip = "";
 	}
 	
-	public String checkPowerUps(String accountId) {
-		return "CheckPowerUps: " +  accountId;
+	public String checkPowerUps(String userId) throws SQLException {
+		conn = DriverManager.getConnection(ip, dbUser, dbPass);
+		String queryUser = "SELECT * from PowerUps WHERE userId = '" + userId + "'" + "and dateUsed IS NULL";
+		try {
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery(queryUser);
+			if(rs.absolute(1))
+			{
+				return rs.toString();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "DB Error";
+		}
+		return "No Powerups";
 	}
 	
-	public String usePowerUp(String accountId, String powerUp) {
-		return "usePowerUp: " +  accountId + "::" + powerUp;
+	public String usePowerUp(String accountId, String powerUp) throws SQLException {
+		String updateSQL = "Update PowerUps SET dateUsed = ? " + "WHERE userID = ?";
+		conn = DriverManager.getConnection(ip, dbUser, dbPass);
+		try {
+			java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+				PreparedStatement prepState = conn.prepareStatement(updateSQL);
+				prepState.setString(1, date.toString());
+				prepState.setString(2, accountId);
+				prepState.executeUpdate();
+				return "User Updated";
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "DB Error";
+		}
+	}
+	
+	public String updateLeaderboardLevel(String accountId, String level) throws SQLException {
+		String updateSQL = "Update Leaderboard SET level = ? " + "WHERE userID = ?";
+		conn = DriverManager.getConnection(ip, dbUser, dbPass);
+		try {
+				PreparedStatement prepState = conn.prepareStatement(updateSQL);
+				prepState.setString(1, level);
+				prepState.setString(2, accountId);
+				prepState.executeUpdate();
+				return "User Updated";
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "DB Error";
+		}
 	}
 }
--- BackendServer/src/main/java/apiServer/AndroidController.java
+++ BackendServer/src/main/java/apiServer/AndroidController.java
@@ -1,4 +1,6 @@
 package apiServer;
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +11,22 @@ public class AndroidController {
 	
 	// Local URL : http://localhost:8080/checkPowerUp?accountId=testDumby
     @RequestMapping("/checkPowerUp")
-    public String checkPowerUps(@RequestParam(value="accountId", defaultValue="0000") String accountId){
+    public String checkPowerUps(@RequestParam(value="accountId", defaultValue="0000") String accountId) throws SQLException{
     	Android and = new Android();
     	return and.checkPowerUps(accountId);
     }
     
 	// Local URL : http://localhost:8080/usePowerUp?accountId=testDumby&powerUp=Fire
     @RequestMapping("/usePowerUp")
-    public String usePowerUp(@RequestParam(value="accountId", defaultValue="0000") String accountId, @RequestParam(value="powerUp", defaultValue="0000") String powerUp){
+    public String usePowerUp(@RequestParam(value="accountId", defaultValue="0000") String accountId, @RequestParam(value="powerUp", defaultValue="0000") String powerUp) throws SQLException{
     	Android and = new Android();
     	return and.usePowerUp(accountId, powerUp);
+    }
+    
+	// Local URL : http://localhost:8080/generateLeader
+    @RequestMapping("/updateLeaderboardLevel")
+    public String updateLeaderboardLevel(@RequestParam(value="accountId", defaultValue="0000") String accountId, @RequestParam(value="level", defaultValue="0000") String level) throws SQLException{
+    	Android and = new Android();
+    	return and.updateLeaderboardLevel(accountId, level);
     }
 }
