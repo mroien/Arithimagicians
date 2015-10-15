a64da1d9a2b1806e0eefc943aabc77384a032848
jacobkinzer01 <jacobkinzer01@gmail.com>
2015-10-14 20:45:22 -0500

    Added in new game button click, popup for no account, map screen, and inventory click

---
+++ Arithimagicians/app/src/main/java/ics499/arithimagicians/InventoryActivity.java
@@ -0,0 +1,14 @@
+package ics499.arithimagicians;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
