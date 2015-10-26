53fb069e8b83e7b50f7acdb0480262f9ec68c5f8
mroien <oien.tim@gmail.com>
2015-10-25 22:28:31 -0500

    changed all to php file and made a few new pages

---
+++ ArithimagiciansWebSite/public/accountInfo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+
+
+?>
+<html>
+
+<head>
+    <title>Account Information</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <h1>Account Information</h1>
+        <div class="scrollwrapper">
+        <img src="../images/Scroll.png" class="scroll">
+            <div class="infoBtnWrapper">
+                <a href="index.php" class="btn-default btn">Home</a>
+                <a href="purchasePowerUps.php" class="btn-default btn">Purchase Power Ups</a>
+                <a href="currentPowerUps.php" class="btn-default btn">Current Power Ups</a>
+                <a href="transactions.php" class="btn-default btn">View Transactions</a>
+                <a href="paymentInfo.php" class="btn-default btn">Payment Information</a>
+                <a href="leaderboard.php" class="btn-default btn">Leaderboard</a>
+                <a href="delete.php" class="btn-danger btn">Delete Account</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ ArithimagiciansWebSite/public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<?php
+require('leftNav.php');
+echo basename($_SERVER['PHP_SELF']);
+?>
+<html>
+
+<head>
+    <title>Arithimiagicins</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <h1 id="title">Arithimagicians</h1>
+
+
+        <img src="../images/Scroll.png" class="scroll">
+
+        <p class="description">Arithimagicians is a mobile game for Android devices. The main character is a talented young spell-caster
+            trying to reach the Arithimagic University of Learnings, Skills, and Smarts (with Wisdom taught where applicable)
+            [P.S. It's applicable everywhere, The Dean] before classes start, using magic dice to cast spells and overcome simple
+            arithmetic problems to defeat opponents along the way.
+        </p>
+    </div>
+</div>
+
+</body>
+</html>
