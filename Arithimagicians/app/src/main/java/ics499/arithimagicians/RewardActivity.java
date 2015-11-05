1ab667efed5334a5955d45fef2c004f05566972a
Brian Shaffer <shafdo@metrostate.edu>
2015-11-05 16:38:31 -0600

    Fixed reward screen to only change next level if the player just completed the latest level

--- Arithimagicians/app/src/main/java/ics499/arithimagicians/RewardActivity.java
+++ Arithimagicians/app/src/main/java/ics499/arithimagicians/RewardActivity.java
@@ -30,46 +30,74 @@ public class RewardActivity extends AppCompatActivity {
     public void setNewLevel(){
         switch(level){
             case "1_1":
-                player.setLevel("1_2");
+                if(player.getLastMap().equalsIgnoreCase("1_1")) {
+                    player.setLevel("1_2");
+                }
                 break;
             case "1_2":
-                player.setLevel("1_3");
+                if(player.getLastMap().equalsIgnoreCase("1_2")) {
+                    player.setLevel("1_3");
+                }
                 break;
             case "1_3":
-                player.setLevel("1_4");
+                if(player.getLastMap().equalsIgnoreCase("1_3")) {
+                    player.setLevel("1_4");
+                }
                 break;
             case "1_4":
-                player.setLevel("1_5");
+                if(player.getLastMap().equalsIgnoreCase("1_4")) {
+                    player.setLevel("1_5");
+                }
                 break;
             case "1_5":
-                player.setLevel("2_1");
+                if(player.getLastMap().equalsIgnoreCase("1_5")) {
+                    player.setLevel("2_1");
+                }
                 break;
             case "2_1":
-                player.setLevel("2_2");
+                if(player.getLastMap().equalsIgnoreCase("2_1")) {
+                    player.setLevel("2_2");
+                }
                 break;
             case "2_2":
-                player.setLevel("2_3");
+                if(player.getLastMap().equalsIgnoreCase("2_2")) {
+                    player.setLevel("2_3");
+                }
                 break;
             case "2_3":
-                player.setLevel("2_4");
+                if(player.getLastMap().equalsIgnoreCase("2_3")) {
+                    player.setLevel("2_4");
+                }
                 break;
             case "2_4":
-                player.setLevel("2_5");
+                if(player.getLastMap().equalsIgnoreCase("2_4")) {
+                    player.setLevel("2_5");
+                }
                 break;
             case "2_5":
-                player.setLevel("3_1");
+                if(player.getLastMap().equalsIgnoreCase("2_5")) {
+                    player.setLevel("3_1");
+                }
                 break;
             case "3_1":
-                player.setLevel("3_2");
+                if(player.getLastMap().equalsIgnoreCase("3_1")) {
+                    player.setLevel("3_2");
+                }
                 break;
             case "3_2":
-                player.setLevel("3_3");
+                if(player.getLastMap().equalsIgnoreCase("3_2")) {
+                    player.setLevel("3_3");
+                }
                 break;
             case "3_3":
-                player.setLevel("3_4");
+                if(player.getLastMap().equalsIgnoreCase("3_3")) {
+                    player.setLevel("3_4");
+                }
                 break;
             case "3_4":
-                player.setLevel("3_5");
+                if(player.getLastMap().equalsIgnoreCase("3_4")) {
+                    player.setLevel("3_5");
+                }
                 break;
 
         }
